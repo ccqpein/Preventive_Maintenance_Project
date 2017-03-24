c887883448d309790deb5c4458efda2a87530799
ccQpein <ccqpein@protonmail.com>
2017-03-24 19:36:49 -0400

    add model and form

--- PM/models.py
+++ PM/models.py
@@ -36,6 +36,68 @@ class MaintenanceSchedule(models.Model):
     ms_tools = models.ManyToManyField(EquipmentTool)
 
 
+class DailyReport(models.Model):
+    dp_name = models.CharField(max_length=50)
+    dp_shift = models.CharField(max_length=50)
+    dp_date = models.DateField()
+    dp_work_performed = models.CharField(max_length=1000)
+    dp_problems_ident = models.CharField(max_length=1000)
+
+
+class Order(models.Model):
+    ord_date = models.DateField()
+    ord_req_by = models.CharField(max_length=50)
+    ord_building = models.CharField(max_length=100)
+    ord_floor = models.CharField(max_length=50)
+    ord_room = models.CharField(max_length=50)
+    ord_supervisor = models.CharField(max_length=100)
+    ord_work_req = models.CharField(max_length=1000)
+    ord_work_ord = models.CharField(max_length=50)
+    ord_date_issue = models.DateField()
+    ord_employee = models.CharField(max_length=100)
+    ord_date_comp = models.DateField()
+    ord_comments = models.CharField(max_length=1000)
+
+
+class CheckList(models.Model):
+    cl_plate_rating = models.CharField(max_length=100)
+    cl_date = models.DateField()
+    cl_operator = models.CharField(max_length=100)
+
+    cl_start_time = models.CharField(max_length=100)
+    cl_stop_time = models.CharField(max_length=100)
+    cl_cool_time = models.CharField(max_length=100)
+
+    cl_oil_level = models.CharField(max_length=100)
+    cl_oil_add = models.CharField(max_length=100)
+
+    cl_radiator_level = models.CharField(max_length=100)
+    cl_radiator_add = models.CharField(max_length=100)
+
+    cl_battery_level = models.CharField(max_length=100)
+    cl_battery_charger = models.CharField(max_length=100)
+
+    cl_oil_pressure = models.CharField(max_length=100)
+    cl_fuel_tank = models.CharField(max_length=100)
+    cl_engine_temp = models.CharField(max_length=100)
+    cl_block_heater = models.CharField(max_length=100)
+    cl_indicator_panel_light = models.CharField(max_length=100)
+
+    cl_amp_1 = models.CharField(max_length=100)
+    cl_amp_2 = models.CharField(max_length=100)
+    cl_amp_3 = models.CharField(max_length=100)
+
+    cl_eptsw = models.CharField(max_length=100)
+    cl_cirnapmpbg = models.CharField(max_length=100)
+    cl_grbblw = models.CharField(max_length=100)
+
+    cl_fire_dampers2 = models.CharField(max_length=150)
+    cl_fire_dampers3 = models.CharField(max_length=150)
+    cl_fire_dampers4 = models.CharField(max_length=150)
+
+    cl_checkbox = models.CharField(max_length=100)
+
+
 class MyUser(AbstractUser):
     phone = models.CharField(max_length=20)
     note = models.CharField(max_length=500)
